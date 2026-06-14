458f5621df7e59fa73db22ca0f1efa9c6b23d126
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-06-14 15:48:19 +0530

    fix

--- ai_model/app.py
+++ ai_model/app.py
@@ -1,4 +1,42 @@
 
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import joblib
+# from fastapi.middleware.cors import CORSMiddleware
+# from data import doctor_map
+
+# # Load trained model and features
+# model = joblib.load("disease_model.pkl")
+# symptoms_list = joblib.load("symptoms.pkl")
+
+# app = FastAPI()
+
+# # CORS for frontend
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["https://mediconnect-xp3k.onrender.com"],  # your React app
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# # Request model
+# class Symptoms(BaseModel):
+#     symptoms: list[str]
+
+# @app.post("/predict")
+# def predict(data: Symptoms):
+#     # Convert symptoms to feature vector
+#     input_vec = [1 if s in data.symptoms else 0 for s in symptoms_list]
+    
+#     disease = model.predict([input_vec])[0]
+#     recommended_doctor = doctor_map.get(disease, "General Physician")
+    
+#     return {
+#         "predicted_disease": disease,
+#         "recommended_doctor": recommended_doctor
+#     }
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
@@ -11,26 +49,33 @@ symptoms_list = joblib.load("symptoms.pkl")
 
 app = FastAPI()
 
-# CORS for frontend
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mediconnect-xp3k.onrender.com"],  # your React app
+    allow_origins=[
+        "http://mediconnect-himanshu.s3-website.ap-south-1.amazonaws.com",
+        "https://mediconnect-himanshu.s3-website.ap-south-1.amazonaws.com",
+        "*"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Request model
 class Symptoms(BaseModel):
     symptoms: list[str]
 
+@app.get("/")
+def home():
+    return {"message": "AI Server Running"}
+
 @app.post("/predict")
 def predict(data: Symptoms):
-    # Convert symptoms to feature vector
     input_vec = [1 if s in data.symptoms else 0 for s in symptoms_list]
-    
+
     disease = model.predict([input_vec])[0]
     recommended_doctor = doctor_map.get(disease, "General Physician")
-    
+
     return {
         "predicted_disease": disease,
         "recommended_doctor": recommended_doctor
